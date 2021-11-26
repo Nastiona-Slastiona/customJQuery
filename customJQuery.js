04d9bfc1ce0ae0a4c6adfdfdbcc8abd8558c6a20
Anastasiya Kruglaya <ariaignis.dragon@gmail.com>
2021-11-26 14:45:20 +0300

    Created 'text' method

--- customJQuery.js
+++ customJQuery.js
@@ -12,7 +12,29 @@ function $(elem) {
             obj.classList.remove(className);
         })
     };
-
+    _innerObject.append = function(...className) {
+        element.forEach(obj => {
+            className.forEach(classToAppend => obj.classList.append(classToAppend));
+        })
+    };
+    _innerObject.remove = function() {
+        element.forEach(obj => {
+            obj.remove();
+        })
+    };
+    _innerObject.text = function(str) {
+        if (str === undefined){
+            let res = '';
+            element.forEach(obj => {
+               res += obj.textContent + '\n';
+            })
+            return res;
+        }
+        element.forEach(obj => {
+            obj.textContent = str;
+        })
+        
+    };
     return _innerObject;
 };
 
